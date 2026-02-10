4678221a9572050b1c1d7915ac3dc7082a73af42
emrgem <emrgem@bergen.org>
2026-02-10 14:54:22 -0500

    Unit 5 Lesson 1 continues

---
+++ Unit5-JS-DOM/Lesson01-The-Bridge/js/script.js
@@ -0,0 +1,9 @@
+// LESSON 01: The Bridge
+// ============================================
+// Test that JavaScript is connected to the page!
+console.log("🎮 GameHub JS is connected!");
+
+//My first DOM Manipulation
+let heroTitle = document.querySelector(`.hero-title`)
+// console.log(heroTitle)
+heroTitle.textContent = "JavaScript is Controlling the page!"
